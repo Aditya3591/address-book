ebb607bed450e6cb2fb76ca6ad40c941784a4e35
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-15 11:02:58 +0530

    [aditya] added: added contact

--- contactlist.py
+++ contactlist.py
@@ -12,7 +12,6 @@ class Contact:
         self.email=email
 
     def display(self):
-
         print("first name: ",self.f_name)
         print("last name: ",self.l_name)
         print("addres: ",self.addres)
@@ -23,5 +22,22 @@ class Contact:
         print("email id: ",self.email)
         
     
-contact1=Contact("aditya","singh","vijaya garden","jsr","jharkhand","831017","8969745425","aditasinghi12@gmail.com")
-contact1.display()
+#contact1=Contact("aditya","singh","vijaya garden","jsr","jharkhand","831017","8969745425","aditasinghi12@gmail.com")
+#contact1.display()
+    def __repr__(self) -> str:
+        return f"\nfirst name : {self.f_name}\nlast name: {self.l_name}\naddress : {self.addres}\ncity : {self.city}\nstate :{self.state}\nzip: {self.zip}\nphn number: {self.phn_num}\nemail: {self.email}"
+        
+class Addressbook:
+
+    def __init__(self):
+        self.contact_list={}
+
+    def add_contact(self, contact_obj):
+        self.contact_list[contact_obj.f_name] = contact_obj
+
+
+if __name__ == '__main__':
+    contact=Contact('aditya','singh','baridh','jamshedpur','jharkhand','831017','8969745425','aditya12@gmail.com')
+    address=Addressbook()
+    address.add_contact(contact)
+    print(address.contact_list)
