4b3e797d228fae5842df7efc0e9a126751475be0
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-20 16:16:46 +0530

    [aditya] added: added multiple address book

--- contactlist.py
+++ contactlist.py
@@ -21,16 +21,14 @@ class Contact:
         print("phone number: ",self.phn_num)
         print("email id: ",self.email)
         
-    
-#contact1=Contact("aditya","singh","vijaya garden","jsr","jharkhand","831017","8969745425","aditasinghi12@gmail.com")
-#contact1.display()
-    def __repr__(self) -> str:
+    # def __repr__(self) -> str:
         
-        return f"\nfirst name : {self.f_name}\nlast name: {self.l_name}\naddress : {self.addres}\ncity : {self.city}\nstate :{self.state}\nzip: {self.zip}\nphn number: {self.phn_num}\nemail: {self.email}"
+    #     return f"\nfirst name : {self.f_name}\nlast name: {self.l_name}\naddress : {self.addres}\ncity : {self.city}\nstate :{self.state}\nzip: {self.zip}\nphn number: {self.phn_num}\nemail: {self.email}"
         
 class Addressbook:
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.contact_list={}
 
     def add_contact(self, contact_obj):
@@ -84,63 +82,126 @@ class Addressbook:
         else:
             print("invalid input")
 
-if __name__ == '__main__':
-     
-    address=Addressbook()
+
+
+class AddressSystem:
+
+    def __init__(self):
+
+        self.address_book_dict={}
     
+    def add_address_book_dict(self,book_obj):
+        self.address_book_dict[book_obj.name] = book_obj
+
+    def delete_address_book(self,name):
+        del self.address_book_dict[name]
+        print("the book is deleted successfully")
+
+    def get_address_book_name(self,name):
+        return self.address_book_dict.get(name)
+    def display(self):
+        return self.address_book_dict
+
+ 
+
+if __name__ == '__main__':
+    print ("Welcome to address book system")
+    multi_book=AddressSystem()
     while True:
-        print("\n1 to add contact")
-        print("2 to update contact")
-        print("3 to display contact")
-        print("4 to del contact") 
-        print("5 to show contact using city or state")
-        print("6 To EXIT")
-
-        choice=input("enter your choice: ")
-
+        print("1 to add address book name ")
+        print("2 to enter an update  a address book name ")
+        print("3 to get address book name ")
+        
+        
+        choice=input("enter your choice")
         if choice=='1':
 
-            f_name=input("Enter your first name: ")
-            l_name=input("Enter your last name: ")
-            addres=input("Enter your address: ")
-            city=input("Enter your city: ")
-            state=input("Enter your state: ")
-            zip=input("Enter your pincode: ")
-            phn_num=input("Enter your phone number: ")
-            email=input("Enter your email id: ")
-        
-
-            contact=Contact(f_name,l_name,addres,city,state,zip,phn_num,email)
-    
-            address.add_contact(contact)
-            print(address.contact_list)
+            book_name=input("enter the address book name :")
+            book = multi_book.get_address_book_name(book_name)
+            if not book:
+                book = Addressbook(book_name)
+            # create contact obj
+            
+            contact=Contact('Aditya','Singh','bradh','jsr','jharkhand','831004','89697454','launda@gmail.com')
+            
+            
+            book.add_contact(contact) 
+            multi_book.add_address_book_dict(book)
+            print(multi_book.display())
+               
 
         elif choice=='2':
+             name=input("enter the address book name")
+             book=multi_book.get_address_book_name(name)
+             book_loop='yes'
+             while(book_loop!='no'):
+                print("\n1 to add contact")
+                print("2 to update contact")
+                print("3 to display contact")
+                print("4 to del contact") 
+                print("5 to show contact using city or state")
+                print("6 To EXIT")
 
-            f_name=input("enter the name of the contact first name to edit the contact details: ")
-            address.update_contact(f_name)
+                choice=input("enter your choice: ")
+
+                if choice=='1':
+
+                    f_name=input("Enter your first name: ")
+                    l_name=input("Enter your last name: ")
+                    addres=input("Enter your address: ")
+                    city=input("Enter your city: ")
+                    state=input("Enter your state: ")
+                    zip=input("Enter your pincode: ")
+                    phn_num=input("Enter your phone number: ")
+                    email=input("Enter your email id: ")
+                
+
+                    contact=Contact(f_name,l_name,addres,city,state,zip,phn_num,email)
+            
+                    book.add_contact(contact)
+                    print(book.contact_list)
+
+                elif choice=='2':
+
+                    f_name=input("enter the name of the contact first name to edit the contact details: ")
+                    book.update_contact(f_name)
+                elif choice=='3':
+
+                    book.display_contacts()
+
+                elif choice=='4':
+
+
+                    f_name=input("enter the first name to delete the contact: ")
+                    book.delete_contact(f_name)
+                elif choice== '5':
+                    
+                    location=input("enter city or state to retrive info: ")
+                    contacts=book.show_cont_with_city_or_state(location)
+
+                    if contacts:
+                            print(f"Contacts in {location}:")
+                            for contact in contacts:
+                                print(contact.display())
+                    else:
+                            print(f"No contacts found in {location}.")
+                
+                
+
+                
+                elif choice=='6':
+                    print("Exiting....")
+                    break
+                else:
+                    print("invalid choice")
+
         elif choice=='3':
-
-            address.display_contacts()
+            book_name=input("enter the addres book name")
+            print(multi_book.get_address_book_name(book_name))
 
         elif choice=='4':
-
-
-            f_name=input("enter the first name to delete the contact: ")
-            address.delete_contact(f_name)
-        elif choice== '5':
-            
-            location=input("enter city or state to retrive info: ")
-            contacts=address.show_cont_with_city_or_state(location)
-
-            if contacts:
-                    print(f"Contacts in {location}:")
-                    for contact in contacts:
-                        print(contact.display())
-            else:
-                    print(f"No contacts found in {location}.")
-        elif choice=='6':
             print("Exiting....")
             break
-        else:
-            print("invalid choice") 
+            
+                        
+        
